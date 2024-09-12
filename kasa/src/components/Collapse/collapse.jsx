1318c6d8de5c79b17251e0e6428d84cc32746393
pommeverte06 <stephaniebp@protonmail.com>
2024-09-12 17:40:23 +0200

    page a propos et 404 ok

---
+++ kasa/src/components/Collapse/collapse.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import arrow from "../../assets/arrow.svg"; // Une seule icône de flèche
+import "./collapse.css";
+
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="collapse">
+      <div className="collapse-header" onClick={() => setIsOpen(!isOpen)}>
+        <h3>{title}</h3>
+        <img
+          src={arrow}
+          alt="flèche d'ouverture"
+          className={`arrow ${isOpen ? "rotate" : ""}`} // Classe rotate appliquée si ouvert
+        />
+      </div>
+      {isOpen && <div className="collapse-content">{content}</div>}
+    </div>
+  );
+}
+
+export default Collapse;
--- kasa/src/pages/about.jsx
+++ kasa/src/pages/about.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
+import Collapse from "../components/Collapse/collapse";
 import aboutBannerImage from "../assets/img-about.png";
 
 function About() {
@@ -7,6 +8,25 @@ function About() {
     <div>
       <main>
         <Banner imageSrc={aboutBannerImage} />
+
+        <div className="collapse-container">
+          <Collapse
+            title="Fiabilité"
+            content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          />
+          <Collapse
+            title="Respect"
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          />
+          <Collapse
+            title="Service"
+            content="La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
+          />
+          <Collapse
+            title="Sécurité"
+            content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien a l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          />
+        </div>
       </main>
     </div>
   );
